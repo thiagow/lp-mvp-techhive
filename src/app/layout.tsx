3d11f2a247ead67bfd6e00f79dbc58446cbc2fcb
Thiago <thiagow.net@gmail.com>
2026-06-19 01:00:46 -0300

    fix: update meta pixel id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'SEU_PIXEL_ID'); /* TODO: Substituir pelo seu ID real */
+            fbq('init', '885762810795367');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -43,7 +43,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=SEU_PIXEL_ID&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=885762810795367&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
